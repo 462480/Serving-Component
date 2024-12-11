c93c1b0d257276fd7075f87da7aa350bb7eec9b5
Silvan-den-Ouden <515074@student.fontys.nl>
2024-12-11 10:19:32 +0100

    added orders!

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScriot : MonoBehaviour
+public class OrderTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<string> orderList = new List<string>();
 
-    // Update is called once per frame
+    private float addOrderTimer = 0f;
+    private float removeOrderTimer = 0f;
+    private int orderCount = 1;
+    private int orderNumber = 1;
+
     void Update()
     {
-        
+        addOrderTimer += Time.deltaTime;
+        removeOrderTimer += Time.deltaTime;
+
+        if (addOrderTimer >= 2f)
+        {
+            if (orderCount <= 5)
+            {
+                string newOrder = "Order " + orderNumber++;
+                orderCount++;
+                orderList.Add(newOrder);
+
+                Debug.Log("Current Orders: " + string.Join(", ", orderList));
+            }
+
+            addOrderTimer = 0f;
+        }
+
+        if (removeOrderTimer >= 20f)
+        {
+            RemoveOrder();
+            removeOrderTimer = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RemoveOrder();
+        }
+    }
+
+    private void RemoveOrder()
+    {
+        if (orderList.Count > 0)
+        {
+            orderList.RemoveAt(0);
+            orderCount--;
+            Debug.Log("Current Orders: " + string.Join(", ", orderList));
+        }
+        else
+        {
+            Debug.Log("No orders to remove.");
+        }
     }
 }
